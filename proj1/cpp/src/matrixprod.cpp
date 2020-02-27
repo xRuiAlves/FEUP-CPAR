636084c885c12866b5edfb887b673d0047f393c8
Rui Alves <up201606746@fe.up.pt>
2020-02-27 14:46:06 +0000

    Refactor matrix prod cpp file

--- proj1/cpp/src/matrixprod.cpp
+++ proj1/cpp/src/matrixprod.cpp
@@ -5,192 +5,163 @@
 #include <time.h>
 #include <cstdlib>
 #include <papi.h>
-
-using namespace std;
+#include <math.h>
+#include <stdlib.h>
 
 #define SYSTEMTIME clock_t
-
  
-void OnMult(int m_ar, int m_br) 
-{
-	
+void multiplyMatrixes(int mat_size, bool in_line = false) {
 	SYSTEMTIME Time1, Time2;
-	
-	char st[100];
-	double temp;
-	int i, j, k;
+	int i, j, k, acc;
+	double* mat1 = (double*) malloc(mat_size * mat_size * sizeof(*mat1));
+	double* mat2 = (double*) malloc(mat_size * mat_size * sizeof(*mat2));
+	double* mat_res = (double*) malloc(mat_size * mat_size * sizeof(*mat_res));
 
-	double *pha, *phb, *phc;
-	
-
-		
-    pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
-	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
-	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
-
-	for(i=0; i<m_ar; i++)
-		for(j=0; j<m_ar; j++)
-			pha[i*m_ar + j] = (double)1.0;
-
-
-
-	for(i=0; i<m_br; i++)
-		for(j=0; j<m_br; j++)
-			phb[i*m_br + j] = (double)(i+1);
-
-
+	for (i = 0; i < mat_size; ++i) {
+		for (j = 0; j < mat_size; ++j) {
+			mat1[i*mat_size + j] = 1.0;
+			mat2[i*mat_size + j] = 1.0 + i;
+		}
+	}
 
     Time1 = clock();
 
-	for(i=0; i<m_ar; i++)
-	{	for( j=0; j<m_br; j++)
-		{	temp = 0;
-			for( k=0; k<m_ar; k++)
-			{	
-				temp += pha[i*m_ar+k] * phb[k*m_br+j];
+	if (in_line) {
+		for (i = 0; i < mat_size; ++i) {	
+			for (k = 0; k < mat_size; ++k) {	
+				for (j = 0; j < mat_size; ++j) {
+					mat_res[i*mat_size + j] += mat1[i*mat_size + k] * mat2[k*mat_size + j];
+				}
 			}
-			phc[i*m_ar+j]=temp;
+		}
+	} else {
+		for (i = 0; i < mat_size; ++i) {	
+			for (j = 0; j < mat_size; ++j) {	
+				acc = 0;
+				for (k = 0; k < mat_size; ++k) {	
+					acc += mat1[i*mat_size + k] * mat2[k*mat_size + j];
+				}
+				mat_res[i*mat_size + j] = acc;
+			}
 		}
 	}
 
-
     Time2 = clock();
-	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
-	cout << st;
+	printf("Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
 
-	cout << "Result matrix: " << endl;
-	for(i=0; i<1; i++)
-	{	for(j=0; j<min(10,m_br); j++)
-			cout << phc[j] << " ";
+	std::cout << "Result matrix:" << std::endl;
+	for(i = 0; i < std::min(10, mat_size); ++i) {
+		std::cout << mat_res[i] << " ";
 	}
-	cout << endl;
+	std::cout << std::endl;
 
-    free(pha);
-    free(phb);
-    free(phc);
-	
-	
+	free(mat1);
+	free(mat2);
+	free(mat_res);
 }
 
-
-void OnMultLine(int m_ar, int m_br)
-{
-    
-    
-}
-
-
-float produtoInterno(float *v1, float *v2, int col)
-{
+float produtoInterno(float *v1, float *v2, int col) {
 	int i;
-	float soma=0.0;	
+	float sum=0.0;	
 
-	for(i=0; i<col; i++)
-		soma += v1[i]*v2[i];
-	
-	return(soma);
-
+	for(i = 0; i < col; ++i) {
+		sum += v1[i] * v2[i];
+	}
+	return(sum);
 }
 
-void handle_error (int retval)
-{
-  printf("PAPI error %d: %s\n", retval, PAPI_strerror(retval));
-  exit(1);
+void handle_error (int ret) {
+	std::cout << "PAPI error " << ret << ": " << PAPI_strerror(ret) << std::endl;
+	exit(1);
 }
 
 void init_papi() {
-  int retval = PAPI_library_init(PAPI_VER_CURRENT);
-  if (retval != PAPI_VER_CURRENT && retval < 0) {
-    printf("PAPI library version mismatch!\n");
-    exit(1);
-  }
-  if (retval < 0) handle_error(retval);
+	int ret = PAPI_library_init(PAPI_VER_CURRENT);
+	if (ret != PAPI_VER_CURRENT && ret < 0) {
+		std::cout << "PAPI library version mismatch!" << std::endl;
+		exit(1);
+	}
+	if (ret < 0) {
+		handle_error(ret);
+	}
 
-  std::cout << "PAPI Version Number: MAJOR: " << PAPI_VERSION_MAJOR(retval)
-            << " MINOR: " << PAPI_VERSION_MINOR(retval)
-            << " REVISION: " << PAPI_VERSION_REVISION(retval) << "\n";
+	std::cout << "PAPI Version Number:" 
+		<< " MAJOR: " << PAPI_VERSION_MAJOR(ret)
+		<< " MINOR: " << PAPI_VERSION_MINOR(ret)
+		<< " REVISION: " << PAPI_VERSION_REVISION(ret) 
+		<< std::endl;
 }
 
 
-int main (int argc, char *argv[])
-{
-
-	char c;
-	int lin, col, nt=1;
-	int op;
-	
+int main (int argc, char *argv[]) {
+	int mat_size, op = 1;
 	int EventSet = PAPI_NULL;
   	long long values[2];
-  	int ret;
 	
+	if (PAPI_library_init(PAPI_VER_CURRENT) != PAPI_VER_CURRENT) {
+		std::cout << "FAIL" << std::endl;
+	}
 
-	ret = PAPI_library_init( PAPI_VER_CURRENT );
-	if ( ret != PAPI_VER_CURRENT )
-		std::cout << "FAIL" << endl;
+	if (PAPI_create_eventset(&EventSet) != PAPI_OK) {
+		std::cout << "ERRO: create eventset" << std::endl;
+	}
 
+	if (PAPI_add_event(EventSet,PAPI_L1_DCM ) != PAPI_OK) {
+		std::cout << "ERRO: PAPI_L1_DCM" << std::endl;
+	}
 
-	ret = PAPI_create_eventset(&EventSet);
-		if (ret != PAPI_OK) cout << "ERRO: create eventset" << endl;
+	if (PAPI_add_event(EventSet,PAPI_L2_DCM) != PAPI_OK) {
+		std::cout << "ERRO: PAPI_L2_DCM" << std::endl;
+	}
 
-
-	ret = PAPI_add_event(EventSet,PAPI_L1_DCM );
-	if (ret != PAPI_OK) cout << "ERRO: PAPI_L1_DCM" << endl;
-
-
-	ret = PAPI_add_event(EventSet,PAPI_L2_DCM);
-	if (ret != PAPI_OK) cout << "ERRO: PAPI_L2_DCM" << endl;
-
-
-	op=1;
 	do {
-		cout << endl << "1. Multiplication" << endl;
-		cout << "2. Line Multiplication" << endl;
-		cout << "Selection?: ";
-		cin >>op;
-		if (op == 0)
+		std::cout << std::endl << "1. Multiplication" << std::endl;
+		std::cout << "2. Line Multiplication" << std::endl;
+		std::cout << "Selection?: ";
+		std::cin >> op;
+
+		if (op == 0) {
 			break;
-		printf("Dimensions: lins cols ? ");
-   		cin >> lin >> col;
-
-
-
-		// Start counting
-		ret = PAPI_start(EventSet);
-		if (ret != PAPI_OK) cout << "ERRO: Start PAPI" << endl;
-
-		switch (op){
-			case 1: 
-				OnMult(lin, col);
-				break;
-			case 2:
-				OnMultLine(lin, col);
-    
-				break;
 		}
 
-  		ret = PAPI_stop(EventSet, values);
-  		if (ret != PAPI_OK) cout << "ERRO: Stop PAPI" << endl;
-  		printf("L1 DCM: %lld \n",values[0]);
-  		printf("L2 DCM: %lld \n",values[1]);
+		std::cout << "Matrix dimension? ";
+   		std::cin >> mat_size;
 
-		ret = PAPI_reset( EventSet );
-		if ( ret != PAPI_OK )
-			std::cout << "FAIL reset" << endl; 
+		// Start counting
+		if (PAPI_start(EventSet) != PAPI_OK) {
+			std::cout << "ERRO: Start PAPI" << std::endl;
+		}
 
+		switch (op){
+		case 1: 
+			multiplyMatrixes(mat_size);
+			break;
+		case 2:
+			multiplyMatrixes(mat_size, true);
+			break;
+		}
 
+  		if (PAPI_stop(EventSet, values) != PAPI_OK) {
+			std::cout << "ERRO: Stop PAPI" << std::endl;
+		}
 
-	}while (op != 0);
+		std::cout << "L1 DCM: " << values[0] << std::endl;
+		std::cout << "L2 DCM: " << values[1] << std::endl;
 
-		ret = PAPI_remove_event( EventSet, PAPI_L1_DCM );
-		if ( ret != PAPI_OK )
-			std::cout << "FAIL remove event" << endl; 
+		if (PAPI_reset(EventSet) != PAPI_OK) {
+			std::cout << "FAIL reset" << std::endl; 
+		}
+	} while (op != 0);
 
-		ret = PAPI_remove_event( EventSet, PAPI_L2_DCM );
-		if ( ret != PAPI_OK )
-			std::cout << "FAIL remove event" << endl; 
+	if (PAPI_remove_event(EventSet, PAPI_L1_DCM) != PAPI_OK) {
+		std::cout << "FAIL remove event" << std::endl; 
+	}
 
-		ret = PAPI_destroy_eventset( &EventSet );
-		if ( ret != PAPI_OK )
-			std::cout << "FAIL destroy" << endl;
+	if (PAPI_remove_event(EventSet, PAPI_L2_DCM) != PAPI_OK) {
+		std::cout << "FAIL remove event" << std::endl;
+	}
 
+	if (PAPI_destroy_eventset( &EventSet ) != PAPI_OK) {
+		std::cout << "FAIL destroy" << std::endl;
+	}
 }
